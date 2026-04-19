f013070210cd98df4ef922bd60e21965478c9c91
PeterVII23 <shirouemiya720@gmail.com>
2026-04-19 23:41:22 +0700

    refactor<QuestionTag>: import gon

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/entity/exam/QuestionTag.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/entity/exam/QuestionTag.java
@@ -1,12 +1,6 @@
 package com.nhom12.enggo_backend.entity.exam;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
