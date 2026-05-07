b7894de52efd0c23ff122e1f8df21fdc97ab7eb8
PeterVII23 <shirouemiya720@gmail.com>
2026-05-07 21:45:45 +0700

    feat<Exam>: them de lay list id cua theme va skill cho exam

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/repository/exam/TagRepository.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/repository/exam/TagRepository.java
@@ -2,6 +2,8 @@ package com.nhom12.enggo_backend.repository.exam;
 
 import com.nhom12.enggo_backend.entity.exam.QuestionTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,19 @@ import java.util.List;
 @Repository
 public interface TagRepository extends JpaRepository<QuestionTag,Long> {
     void deleteByQuestionId(Integer questionId);
+
+    @Query("""
+        SELECT DISTINCT qt.id.themeId
+        FROM QuestionTag qt
+        JOIN ExamQuestion eq ON qt.id.questionId = eq.id.questionId
+        WHERE eq.exam.id = :id
+    """)
+    List<Integer> findDistinctThemeIdsByExamId(@Param("id") Integer id);
+
+    @Query("""
+        SELECT DISTINCT qt.id.skillId FROM QuestionTag qt
+        JOIN ExamQuestion eq ON qt.id.questionId = eq.id.questionId
+        WHERE eq.exam.id = :id
+    """)
+    List<Integer> findDistinctSkillIdsByExamId(@Param("id") Integer id);
 }
