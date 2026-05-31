57ddd149739335509db19c174d7ccf4d2ccb0413
PeterVII23 <shirouemiya720@gmail.com>
2026-05-31 22:54:14 +0700

    feat<ExamAttemptController>: tao repo, mapper ho tro service va api

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/mapper/exam/ExamAttemptMapper.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/mapper/exam/ExamAttemptMapper.java
@@ -2,15 +2,14 @@ package com.nhom12.enggo_backend.mapper.exam;
 
 import com.nhom12.enggo_backend.dto.request.exam.ExamAnswerRequest;
 import com.nhom12.enggo_backend.dto.request.exam.ExamSubmitRequest;
-import com.nhom12.enggo_backend.dto.response.exam.ExamDetailResponse;
-import com.nhom12.enggo_backend.dto.response.exam.ExamSubmitResponse;
-import com.nhom12.enggo_backend.entity.exam.ExamAttempt;
-import com.nhom12.enggo_backend.entity.exam.ExamAttemptDetail;
-import com.nhom12.enggo_backend.entity.exam.Question;
-import com.nhom12.enggo_backend.entity.exam.QuestionOption;
+import com.nhom12.enggo_backend.dto.request.exam.FillBlankSubmitRequest;
+import com.nhom12.enggo_backend.dto.response.exam.*;
+import com.nhom12.enggo_backend.entity.exam.*;
 import com.nhom12.enggo_backend.service.exam.ScoreCheck;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ExamAttemptMapper {
@@ -32,4 +31,18 @@ public interface ExamAttemptMapper {
     @Mapping(target = "examId", source = "exam.id")
     @Mapping(target = "totalQuestions", source = "exam.totalQuestions")
     ExamSubmitResponse toExamSubmitResponse(ExamAttempt examAttempt);
+
+    List<ExamSubmitResponse> toExamSubmitResponses(List<ExamAttempt> examAttempts);
+
+    default MultipleOptionResultResponse toMultipleOptionResultResponse (ExamAttemptDetail detail, QuestionOption option) {
+        Integer selectedId = detail.getSelectedOption() != null ? detail.getSelectedOption().getId() : null;
+
+        MultipleOptionResultResponse response = new MultipleOptionResultResponse();
+        response.setId(option.getId());
+        response.setOptionText(option.getOptionText());
+        response.setCorrect(option.isCorrect());
+        response.setSelected(option.getId().equals(selectedId));
+
+        return response;
+    };
 }
--- enggo-backend/src/main/java/com/nhom12/enggo_backend/repository/exam/ExamAttemptRepository.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/repository/exam/ExamAttemptRepository.java
@@ -1,10 +1,26 @@
 package com.nhom12.enggo_backend.repository.exam;
 
 import com.nhom12.enggo_backend.entity.exam.ExamAttempt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExamAttemptRepository extends JpaRepository<ExamAttempt, Integer> {
     boolean existsByUserIdAndCompleteFalse (Integer userId);
+
+    @Query("SELECT a.id FROM ExamAttempt a WHERE a.user.id = :userId")
+    Page<Integer> findAllIds (Pageable pageable,@Param("userId") Integer userId);
+
+    @Query("""
+            SELECT a FROM ExamAttempt a
+            WHERE a.id IN :ids
+            ORDER BY a.completedAt
+            """)
+    List<ExamAttempt> findByIds (@Param("ids") List<Integer> ids);
 }
