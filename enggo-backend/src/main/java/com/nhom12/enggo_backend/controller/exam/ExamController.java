32b70586bbc2e16257b9046d90c4629ad629a1bb
PeterVII23 <shirouemiya720@gmail.com>
2026-06-06 21:25:17 +0700

    feat<ExamController>: them api random exam

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/controller/exam/ExamController.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/controller/exam/ExamController.java
@@ -2,17 +2,21 @@ package com.nhom12.enggo_backend.controller.exam;
 
 import com.nhom12.enggo_backend.dto.request.ApiResponse;
 import com.nhom12.enggo_backend.dto.request.exam.ExamSubmitRequest;
+import com.nhom12.enggo_backend.dto.request.exam.RandomBlueprintRequest;
 import com.nhom12.enggo_backend.dto.response.PageResponse;
 import com.nhom12.enggo_backend.dto.response.exam.ExamDetailResponse;
 import com.nhom12.enggo_backend.dto.response.exam.ExamDisplayResponse;
 import com.nhom12.enggo_backend.dto.response.exam.ExamResponse;
 import com.nhom12.enggo_backend.dto.response.exam.ExamSubmitResponse;
+import com.nhom12.enggo_backend.entity.identity.User;
 import com.nhom12.enggo_backend.repository.exam.ExamAttemptRepository;
 import com.nhom12.enggo_backend.repository.exam.ExamRepository;
 import com.nhom12.enggo_backend.service.exam.ExamAttemptService;
+import com.nhom12.enggo_backend.service.exam.ExamGenerationService;
 import com.nhom12.enggo_backend.service.exam.ExamService;
 import com.nhom12.enggo_backend.service.exam.ExcelImportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +30,7 @@ public class ExamController {
     private final ExcelImportService  excelImportService;
     private final ExamService examService;
     private final ExamAttemptService attemptService;
+    private final ExamGenerationService examGenerationService;
 
     @PostMapping("/import")
     ApiResponse<ExamDetailResponse> importExamExcel(@RequestParam("file") MultipartFile file) throws IOException {
@@ -78,6 +83,15 @@ public class ExamController {
             @PathVariable("attemptId") Integer attemptId) {
         return ApiResponse.<ExamSubmitResponse>builder()
                 .result(attemptService.submitExam(examSubmitRequest, examId, attemptId))
+                .build();
+    }
+
+    @PostMapping("/random")
+    public ApiResponse<ExamResponse> getOrCreateRandomExam (
+            @RequestBody RandomBlueprintRequest request
+            ) {
+        return ApiResponse.<ExamResponse>builder()
+                .result(examGenerationService.getOrGenerateExamResponse(request))
                 .build();
     }
 }
