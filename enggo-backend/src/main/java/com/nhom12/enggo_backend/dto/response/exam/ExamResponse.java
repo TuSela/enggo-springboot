b17d99332b2e00547704a1face0feb5f3d54b638
PeterVII23 <shirouemiya720@gmail.com>
2026-06-06 21:45:57 +0700

    fix<ExamResponse>: them annotation builder

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/dto/response/exam/ExamResponse.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/dto/response/exam/ExamResponse.java
@@ -1,12 +1,14 @@
 package com.nhom12.enggo_backend.dto.response.exam;
 
 import com.nhom12.enggo_backend.dto.response.UserMinimalResponse;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 public class ExamResponse {
     Integer id;
     String title;
