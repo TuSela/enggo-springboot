706b48d0d264453f3533bccb3043d8051177821e
PeterVII23 <shirouemiya720@gmail.com>
2026-06-27 13:04:42 +0700

    feat(theme mapper): ignore themeImage de ko hien loi

--- enggo-backend/src/main/java/com/nhom12/enggo_backend/mapper/exam/ThemeMapper.java
+++ enggo-backend/src/main/java/com/nhom12/enggo_backend/mapper/exam/ThemeMapper.java
@@ -4,11 +4,15 @@ import com.nhom12.enggo_backend.dto.request.exam.ThemeRequest;
 import com.nhom12.enggo_backend.dto.response.exam.ThemeResponse;
 import com.nhom12.enggo_backend.entity.exam.Theme;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.web.multipart.MultipartFile;
 
 @Mapper(componentModel = "spring")
 public interface ThemeMapper {
+    @Mapping(target = "themeImage", ignore = true)
     Theme toTheme(ThemeRequest theme);
     ThemeResponse toThemeResponse(Theme theme);
+    @Mapping(target = "themeImage", ignore = true)
     void updateTheme(@MappingTarget Theme theme, ThemeRequest themeRequest);
 }
