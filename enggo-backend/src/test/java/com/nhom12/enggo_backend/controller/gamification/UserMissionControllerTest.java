8cb8052938466910c8c92d92b01c418f4a6d54d7
DucTu <tusela022@gmail.com>
2026-06-29 23:29:44 +0700

    fix cicd

--- enggo-backend/src/test/java/com/nhom12/enggo_backend/controller/gamification/UserMissionControllerTest.java
+++ enggo-backend/src/test/java/com/nhom12/enggo_backend/controller/gamification/UserMissionControllerTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -33,10 +33,10 @@ class UserMissionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private UserMissionService userMissionService;
 
-    @MockBean
+    @MockitoBean
     private UserService userService;
 
     private final Integer userId = 1;
